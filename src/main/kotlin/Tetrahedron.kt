dbcdf4a191c4ea71df1544c8c19468cb90a0cbff
MaxBuster <maxbuster380@gmail.com>
2023-04-01 19:14:52 +0200

    Tetrahedron can give barycentric coordinates

--- src/main/kotlin/Tetrahedron.kt
+++ src/main/kotlin/Tetrahedron.kt
@@ -14,6 +14,36 @@ class Tetrahedron {
 
     // INSTANCE METHODS
 
+    /**
+     * Creates 4 different tetrahedrons that all have the given point as a vertice, the other vertices are vertices of the called tetrahedron.
+     *  (clientVector, vertice1, vertice2, vertice3),
+     *  (clientVector, vertice0, vertice2, vertice3),
+     *  (clientVector, vertice0, vertice1, vertice3),
+     *  (clientVector, vertice0, vertice1, vertice2)
+     */
+    fun cut(clientVector:ThreeDVector):List<Tetrahedron> {
+        val tV0 = Tetrahedron(clientVector, vertices[1], vertices[2], vertices[3])
+        val tV1 = Tetrahedron(clientVector, vertices[0], vertices[2], vertices[3])
+        val tV2 = Tetrahedron(clientVector, vertices[0], vertices[1], vertices[3])
+        val tV3 = Tetrahedron(clientVector, vertices[0], vertices[1], vertices[2])
+
+        return listOf(tV0, tV1, tV2, tV3)
+    }
+
+    /**
+     * Returns the barycentric coordinates of a given point based on the tetrahedron
+     */
+    fun getBarycentricCoordinates(clientVector : ThreeDVector):List<Double> {
+        val cutPieces = cut(clientVector)
+
+        val coordinates : MutableList<Double> = mutableListOf()
+        for(i in cutPieces.indices) {
+            coordinates += (cutPieces[i].volume / this.volume)
+        }
+
+        return coordinates
+    }
+
     fun getVertices():List<ThreeDVector> {
         return vertices
     }
