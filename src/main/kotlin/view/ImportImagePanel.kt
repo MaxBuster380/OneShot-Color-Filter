760a8a3e885cf28177f4359aabe0ff25b0479998
MaxBuster <maxbuster380@gmail.com>
2023-08-02 11:10:52 +0200

    Added ImportImagePanel

---
+++ src/main/kotlin/view/ImportImagePanel.kt
@@ -0,0 +1,34 @@
+package view
+
+import java.awt.Color
+import java.awt.GridLayout
+import javax.swing.BorderFactory
+import javax.swing.JButton
+import javax.swing.JPanel
+import javax.swing.JTextField
+
+class ImportImagePanel:JPanel() {
+
+	private val importedFilePathTextField = createImportedFilePathTextField()
+	private val pickNewFileButton = createPickNewFileButton()
+
+	init {
+		layout = GridLayout(2,1)
+
+		add(importedFilePathTextField)
+		add(pickNewFileButton)
+
+		border = BorderFactory.createLineBorder(Color(0, 0, 0), 4)
+	}
+
+	private fun createImportedFilePathTextField():JTextField {
+		val res = JTextField("PLACEHOLDER INPUT IMAGE PATH")
+		res.isEditable = false
+		return res
+	}
+
+	private fun createPickNewFileButton(): JButton {
+		val res = JButton("PLACEHOLDER IMPORT BUTTON")
+		return res
+	}
+}
