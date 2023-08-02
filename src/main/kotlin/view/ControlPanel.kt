2460da70757e61de931554107730f890a79c4c92
MaxBuster <maxbuster380@gmail.com>
2023-08-02 11:10:45 +0200

    Added ControlPanel

---
+++ src/main/kotlin/view/ControlPanel.kt
@@ -0,0 +1,16 @@
+package view
+
+import java.awt.Color
+import java.awt.GridLayout
+import javax.swing.BorderFactory
+import javax.swing.JPanel
+
+class ControlPanel : JPanel() {
+	init {
+		layout = GridLayout(3,1)
+
+		border = BorderFactory.createLineBorder(Color(128, 128, 128), 4)
+
+		add(ImportImagePanel())
+	}
+}
